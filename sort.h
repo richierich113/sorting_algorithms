c6d5ee39023e6592d0198144be7d91ec98d68c09
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-27 11:03:04 +0000

    Update sort.h

--- sort.h
+++ sort.h
@@ -26,7 +26,7 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void swap_nodes(listint_t **h, listint_t **first_node, listint_t *sec_node);
+void swapper(listint_t **h, listint_t **first_node, listint_t *sec_node);
 void selection_sort(int *array, size_t size);
 
 
